c8408f318c6052a537f13ca5086708a198ae0547
Hugo PAGNIEZ <hugo.pagniez@gmail.com>
2023-12-04 13:55:05 +0100

    day 4

---
+++ src/AdventOfCode2023.Day4/Part1.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2023.Day4;
+
+public static class Part1
+{
+    public static void Run(string[] lines)
+    {
+        int total = 0;
+        //line format: Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        foreach (string line in lines)
+        {
+            List<int> leftNumbers = line.Substring(line.IndexOf(':') + 1, line.IndexOf('|') - line.IndexOf(':') - 1)
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> rightNumbers = line[(line.IndexOf('|') + 1)..]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> results = rightNumbers.Intersect(leftNumbers).ToList();
+
+            total += results.Count == 0 ? 0 : (int)Math.Pow(2, results.Count - 1);
+        }
+        Console.WriteLine("Part 1: " + total);
+    }
+}
---
+++ src/AdventOfCode2023.Day4/Part2.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2023.Day4;
+
+public static class Part2
+{
+    public static void Run(string[] lines)
+    {
+        Dictionary<int, int> finalRes = new Dictionary<int, int>();
+
+        for (int i = 1; i < lines.Length + 1; i++)
+        {
+            finalRes[i] = 1;
+        }
+        
+        //line format: Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        foreach (string line in lines)
+        {
+            int cardNo = int.Parse(line.Substring(line.IndexOf(' '), line.IndexOf(':') - line.IndexOf(' ')));
+            
+            List<int> leftNumbers = line.Substring(line.IndexOf(':') + 1, line.IndexOf('|') - line.IndexOf(':') - 1)
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> rightNumbers = line[(line.IndexOf('|') + 1)..]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> results = rightNumbers.Intersect(leftNumbers).ToList();
+
+            for (int i = 1; i < results.Count + 1; i++)
+            {
+                finalRes[cardNo + i] += finalRes[cardNo];
+            }
+        }
+        
+        Console.WriteLine("Part 2: " + finalRes.Sum(x => x.Value));
+    }
+}
